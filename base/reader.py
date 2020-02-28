3d586892267a84516964d4672ac2055368131273
QuentinN42 <quentin.lieumont@gmail.com>
2020-02-28 13:03:44 +0100

    jupyter refactor

--- base/reader.py
+++ base/reader.py
@@ -1,8 +1,8 @@
 """
 Basic auto cipher and decipher
 """
-from .abstract_classes import Shift
-from .functions import search_shift
+from .abstract_classes import Shift, MultiFunc, LinspaceCut
+from .functions import search_shift, zipMerge
 
 
 def decipher(filename: str, dec: callable):
@@ -30,3 +30,13 @@ def auto_shift(filename: str):
     result = Shift(search_shift(content))(content)
 
     return result
+
+
+def vignere(filename: str, nb_cle: int):
+    with open(filename, "r") as f:
+        content = f.read()
+
+    cut = LinspaceCut(nb_cle)
+    keys = map(search_shift, cut(content))
+    funcs = map(Shift, keys)
+    return decipher(filename, MultiFunc(cut, funcs, zipMerge))
