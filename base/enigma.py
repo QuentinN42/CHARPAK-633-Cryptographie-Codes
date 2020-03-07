d2e8e01d1e70941fa52d57fdbbcf14f6f1ffa5e2
QuentinN42 <quentin.lieumont@gmail.com>
2020-03-07 15:00:07 +0100

    EnigmaDecode

--- base/enigma.py
+++ base/enigma.py
@@ -25,3 +25,12 @@ class Enigma:
                 self.keys[2] += 1
                 if self.keys[2] == 256:
                     self.keys[2] = 0
+
+
+class EnigmaDecode(Enigma):
+    def code_one(self, letter):
+        pos = ord(letter)
+        for i, k in reversed(list(enumerate(self.keys))):
+            pos = (self.gears[i].index((pos + k) % 256) - k + 256) % 256
+        self.rotate()
+        return chr(pos)
